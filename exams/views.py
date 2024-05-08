63aeab978cd28657c56ed9297f856a3cd541d05b
KimYoungHoo <chrishoho8902@gmail.com>
2024-05-08 18:30:19 +0900

    todo

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+TODO:
+1. [POST] /exams : 시험 생성. 어드민 only.
+2. [POST] /exams/slots : 시험 슬롯 생성. admin only.
+3. [GET] /exams/slots : 시험 슬롯 목록 조회. 예약 가능여부로 필터링 기능. 페이지네이션. 
+"""
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+TODO:
+1. [PATCH] /reservations/{id}/confirm/{status} : 예약 확정. 어드민 only. race condition 고려
+2. [POST] /reservations : 예약 생성. 로그인 필수.
+3. [GET] /reservations : 예약 조회. 어드민 아니면 자기꺼만 조회, 어드민이면 전체 조회. 페이지네이션
+4. [DELETE] /reservations : 예약 삭제(취소).
+"""
